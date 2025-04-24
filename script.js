8c440e9d558b4d6f2a99e4e7b7697a1189f50b7a
Shaseram <107533892+Shaseram@users.noreply.github.com>
2025-04-23 23:57:11 -0400

    finished project with extra features

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
 const container = document.querySelector("#container");
 const boton = document.createElement("button");
+boton.textContent = 'Change NxN';
+boton.id = 'button';
 
-function gridDisplay(row,column) {
-    
-    for(let i = 1; i <= row; i++){
+const gridSquare = document.createElement("div");
+gridSquare.id = 'grid-square';
 
-        let row = document.createElement("div");
-        row.classList.add("row");
+function gridDisplay(n) {
 
-        for(let j = 1; j <= column; j++) {
+    const squareSize = 600 / n
+
+    for(let i = 0; i < n; i++) {
+        const squareRow = document.createElement("div");
+        squareRow.classList.add("row-content");
+        for(let j = 0; j < n; j++) {
             let square = document.createElement("div");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
             square.classList.add("square");
-            square.addEventListener("mouseover", () => {
-                square.classList.add("hover");
-            })
-            row.appendChild(square);
+            
+            let v1 = Math.floor(Math.random() * 256);
+            let v2 = Math.floor(Math.random() * 256);
+            let v3 = Math.floor(Math.random() * 256);
+            let opacity = 0.0;
 
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = `rgb(${v1},${v2},${v3})`;
+                
+                if (opacity <= 0.9) {
+                    opacity += 0.1;
+                }
+                
+                square.style.opacity = `${opacity}`;
+            });
+
+            squareRow.appendChild(square);
         }
 
-        container.appendChild(row);
+        gridSquare.appendChild(squareRow);
     }
 }
 
+function startSquare() {
+    
+    boton.addEventListener("click", () => {
+        let n = prompt("Indicate the NxN number");
+        while(n > 100 || n < 1) {
+            n = prompt("Indicate the N rows with valid domain");
+        }
+    
+        gridSquare.textContent = '';
+        gridDisplay(n);
+    });
 
-boton.id = 'bt-c';
-boton.textContent = 'Change NxN';
- 
-boton.addEventListener("click", () => {
-    let n = prompt("Indicate the N rows");
-    while(n > 100 || n < 1) {
-        n = prompt("Indicate the N rows with valid domain");
-    }
-    let m = prompt("Indicate the M columns");
-    while(m > 100 || m < 1) {
-        m = prompt("Indicate the m columns with valid domain");
-    }
-    container.textContent = '';
     container.appendChild(boton);
-    gridDisplay(n,m)
-})
+    container.appendChild(gridSquare);  
 
-container.appendChild(boton);
+    gridDisplay(10);
+}
 
-
-gridDisplay(10,10);
+startSquare();
 
